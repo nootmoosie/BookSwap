7f2ca253ad524e01599b2e3f45d74547c9c20ad8
Nathan Masi <nmasi@umass.edu>
2018-03-25 15:11:44 -0400

    Added basic book model (may add more details).

--- project2/WebDevBois/BookSwap/models.py
+++ project2/WebDevBois/BookSwap/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+
 
 # Basic Model layout:
 
@@ -39,3 +41,26 @@ class Genre(models.Model):
         String for representing the Model object (in Admin site etc.)
         """
         return self.name
+
+class Book(models.Model):
+    """
+    Model representing a book (but not a specific copy of a book).
+    """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+    
+    
+    def get_absolute_url(self):
+        """
+        Returns the url to access a detail record for this book.
+        """
+        return reverse('book-detail', args=[str(self.id)])
