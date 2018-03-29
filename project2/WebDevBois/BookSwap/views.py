2630eb4990b79a1152c93061fce3e967eeb4dd06
Nathan Masi <nmasi@umass.edu>
2018-03-28 21:08:56 -0400

    Added views for index page, got three books to display but still struggling w genre and img.

--- project2/WebDevBois/BookSwap/views.py
+++ project2/WebDevBois/BookSwap/views.py
@@ -7,18 +7,12 @@ def index(request):
     """
     View function for home page of site.
     """
-    # Generate counts of some of the main objects
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
-    # Available books (status = 'a')
-    num_instances_available=BookInstance.objects.all().count()
-    num_authors=Author.objects.count()  # The 'all()' is implied by default.
-    
+    queryset = BookInstance.objects.all()[:3]
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'queryset':queryset},
     )
 
 def browse(request):
