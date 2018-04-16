a85094e8ec482e3316e17b36fd8a98143d66bbdb
Timothy McNamara <timmy.mc.2@gmail.com>
2018-04-16 00:26:38 -0400

    re-removed the Profile books_offered foreign key after consulting with Jamie

--- project3/WebDevBois/BookSwap/models.py
+++ project3/WebDevBois/BookSwap/models.py
@@ -152,7 +152,7 @@ class Profile(models.Model):
 	bio = models.CharField(max_length=200, help_text="Enter a short bio.")
 
     
-	books_offered = models.ForeignKey('BookInstance', on_delete=models.CASCADE, null=True)
+	# books_offered = models.ForeignKey('BookInstance', on_delete=models.CASCADE, null=True)
 	books_wanted = models.ManyToManyField(Book) 
 
 	# class Meta:
